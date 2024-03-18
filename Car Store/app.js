83704896cd3ec2fbbc43879166d26b32c0bc53cc
Muhammad Burhan <136472738+muhammadburhan110@users.noreply.github.com>
2024-03-18 15:41:33 -0700

    Add files via upload

---
+++ Car Store/app.js
@@ -0,0 +1,288 @@
+var carlist = {
+    SUZUKI: {
+        MEHRAN:{
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfVyJMbYNJ-gHkhUW10oFH3vrObupKM9ycl1LRLxqbTQ&s",
+            model:'2020',
+            seatcapacity: '5 person',
+            fueltype: 'petrol',
+            transmission:'Type manual',
+            Mileage:'14 to 18 kM/LITER',
+            wheel:'12 in',
+            price:'1,700,000 PKR',
+            color:['red','blue','green']  
+        },
+        ALTO:{
+            image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgBQb1jRQlxCAs308sKRgAw_IVIw1OmY-f3A5hxYAsww&s",
+            model:'2022',
+            seatcapacity: '6 person',
+            fueltype: 'Diesel',
+            transmission:'Type Auto',
+            Mileage:'15 to 19 kM/LITER',
+            wheel:'15 in',
+            price:'1,900,000 PKR',
+            color:['blue','pink','brown']  
+        },
+        CULTUS:{
+            image:"https://imgcdn.zigwheels.pk/large/gallery/color/13/124/suzuki-cultus-color-704737.jpg",
+            model:'2023',
+            seatcapacity: '8 person',
+            fueltype: 'petrol',
+            transmission:'Type manual',
+            Mileage:'19 to 22kM/LITER',
+            wheel:'17 in',
+            price:'2,000,000 PKR',
+            color:['red','white','grey']  
+        },
+        SWIFT:{
+            image:"https://cache1.pakwheels.com/system/car_generation_pictures/7441/original/SWIFT.jpg?1677750438",
+            model:'2024',
+            seatcapacity: '9 person',
+            fueltype: 'petrol',
+            transmission:'Type auto',
+            Mileage:'21 to 26 kM/LITER',
+            wheel:'19 in',
+            price:'2,200,000 PKR',
+            color:['white','silver','blue']  
+        }
+    },
+    HONDA:{
+        CIVIC:{
+            image:"https://cache4.pakwheels.com/system/car_generation_pictures/7370/original/Cover.jpg?1677570254",
+            model:'2024',
+            seatcapacity: '6 person',
+            fueltype: 'petrol',
+            transmission:'Type auto',
+            Mileage:'22 to 28 kM/LITER',
+            wheel:'19 in',
+            price:'8,700,000 PKR',
+            color:['white','silver','black']  
+        },
+        CITY:{
+            image:"https://cache1.pakwheels.com/system/car_generation_pictures/6425/original/Honda_City_Front.jpg?1651424945",
+            model:'2023',
+            seatcapacity: '5 person',
+            fueltype: 'petrol',
+            transmission:'Type Auto',
+            Mileage:'20 to 26 kM/LITER',
+            wheel:'17 in',
+            price:'4,500,000 PKR',
+            color:['black','blue','white']  
+        },
+        BRV:{
+            image:"https://luxuryrentacar.pk/wp-content/uploads/2023/07/Honda-BRV-removebg-preview.png",
+            model:'2021',
+            seatcapacity: '6 person',
+            fueltype: 'petrol',
+            transmission:'Type manual',
+            Mileage:'19 to 23kM/LITER',
+            wheel:'16 in',
+            price:'3,500,000 PKR',
+            color:['white','grey','black']  
+        },
+        hRV:{
+            image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNW1t8SmA23TW7VDvWhv1aqBLblZNK7UVgL7jY7mPTIg&s",
+            model:'2023',
+            seatcapacity: '6 person',
+            fueltype: 'petrol',
+            transmission:'Type auto',
+            Mileage:'21 to 26 kM/LITER',
+            wheel:'21 in',
+            price:'45,00,000 PKR',
+            color:['white','black','red']  
+        }     
+    },
+    TOYOTA:{
+        COROLA:{
+            image:"https://i0.wp.com/www.toyotaravi.com/wp-content/uploads/2015/12/1.8-white-scaled.jpg?fit=2560%2C1705&ssl=1",
+            model:'2021',
+            seatcapacity: '7 person',
+            fueltype: 'petrol',
+            transmission:'Type auto',
+            Mileage:'21 to 26 kM/LITER',
+            wheel:'18 in',
+            price:'5,500,000 PKR',
+            color:['blue','purple','white']  
+        },
+        FORTUNER:{
+            image:"https://cache4.pakwheels.com/system/car_generation_pictures/7335/original/Fortuner_-_PNG.png?1677568997",
+            model:'2023',
+            seatcapacity: '8 person',
+            fueltype: 'petrol',
+            transmission:'Type Auto',
+            Mileage:'22 to 29 kM/LITER',
+            wheel:'22 in',
+            price:'9,000,000 PKR',
+            color:['grey','black','white']  
+        },
+        LANDCRUISER:{
+            image:"https://www.ccarprice.com/products/Toyota_Land_Cruiser_LC300_GXl_2023.jpg",
+            model:'2024',
+            seatcapacity: '9 person',
+            fueltype: 'petrol',
+            transmission:'Type auto',
+            Mileage:'23 to 29kM/LITER',
+            wheel:'24 in',
+            price:'9,900,000 PKR',
+            color:['black','white','grey']  
+        },
+        GLANZA:{
+            image:"https://imgd-ct.aeplcdn.com/1056x660/n/xacauua_1563333.jpg?q=80",
+            model:'2022',
+            seatcapacity: '6 person',
+            fueltype: 'petrol',
+            transmission:'Type manual',
+            Mileage:'22 to 26 kM/LITER',
+            wheel:'17 in',
+            price:'4,800,000 PKR',
+            color:['white','silver','red']  
+        }        
+    },
+    HYUNDAI:{
+        TUCSON:{
+            image:"https://cache3.pakwheels.com/system/car_generation_pictures/6022/original/Tucson_-_PNG.png?1635948541",
+            model:'2023',
+            seatcapacity: '7 person',
+            fueltype: 'petrol',
+            transmission:'Type Auto',
+            Mileage:'16 to 21 kM/LITER',
+            wheel:'18 in',
+            price:'9,000,000 PKR',
+            color:['blue','white','green']  
+        },
+        SANTAFE:{
+            image:"https://cars.usnews.com/static/images/Auto/izmo/i5445/2016_hyundai_santa_fe_angularfront.jpg",
+            model:'2021',
+            seatcapacity: '6 person',
+            fueltype: 'Diesel',
+            transmission:'Type Auto',
+            Mileage:'15 to 21 kM/LITER',
+            wheel:'19 in',
+            price:'8,000,000 PKR',
+            color:['white','yellow','brown']  
+        },
+        KONA:{
+            image:"https://inv.assets.ansira.net/ChromeColorMatch/us/TRANSPARENT_cc_2023HYS060061_01_1280_Y7H.png",
+            model:'2023',
+            seatcapacity: '5 person',
+            fueltype: 'petrol',
+            transmission:'Type auto',
+            Mileage:'10 to 14kM/LITER',
+            wheel:'18 in',
+            price:'7,870,000 PKR',
+            color:['blue','red','white']  
+        },
+        PALISADE:{
+            image:"https://maquinarias.pe/wp-content/uploads/2021/08/palisade.png",
+            model:'2023',
+            seatcapacity: '5 person',
+            fueltype: 'petrol',
+            transmission:'Type auto',
+            Mileage:'12 to 18 kM/LITER',
+            wheel:'16 in',
+            price:'8,670,000 PKR',
+            color:['red','white','grey']  
+        },
+    }
+}
+
+var main = document.getElementById("main");
+var brand = document.getElementById("brand");
+var model = document.getElementById("model");
+brand.innerHTML += `<option>Select Brand name</option>`
+model.innerHTML += `<option>Select model name</option>`
+
+function renderAllCars(){
+    
+    for(var key in carlist){
+    
+        console.log(key)
+        brand.innerHTML += `<option value='${key}'>${key}</option>`
+    
+        for(var key1 in carlist[key]){
+            
+            console.log(key1)
+    
+            var obj = carlist[key][key1];
+            var colorDiv ="";
+            for (var i=0; i < obj.color.length; i++){
+                colorDiv +=`<div class='${obj.color[i]}'></div>`
+            }
+    
+            main.innerHTML += `
+            <div class="col-12 col-sm-12 col-md-6 col-lg-3 mb-2">
+    
+                <div class="card" style="width: 18rem;">
+                    <img src= "${obj.image || "https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/images/car-reviews/first-drives/legacy/rolls_royce_phantom_top_10.jpg?itok=XjL9f1tx"}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">${key1.toUpperCase()} (${obj.model})</h5>
+                      <p class="card-text">
+                      ${obj.Mileage}
+                      <br />
+                      ${obj.transmission}
+                      </p>
+                      <h6>${obj.price}<\h6>
+                      <div class="d-flex gap-2">
+                      ${colorDiv}
+                      </div>  
+    
+                     
+                    </div>
+                  </div>
+    
+            </div>
+            
+            `    
+    
+            }
+        }
+}
+
+renderAllCars()
+
+function setModel(){
+    model.innerHTML ="";
+    model.innerHTML += `<option>Select model name</option>`
+
+
+    for(var key in carlist[brand.value]){
+        model.innerHTML += `<option value ='${key}'>${key}</option>`
+    }
+}    
+
+var carDetail = document.getElementById('car-detail');
+function searchCar(){
+    main.innerHTML="";
+    main.style.display ="none";
+    carDetail.style.display = "flex";
+
+    var car = carlist[brand.value][model.value];
+
+    carDetail.innerHTML = ` 
+    <div class="card mb-3" >
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="${car.image || "https://www.samaa.tv/images/suzuki-mehran-latest-price.jpg"}" class="img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">${model.value.toUpperCase()}</h5>
+          <p class="card-text">
+                  ${car.Mileage}
+                  <br />
+                  ${car.transmission}
+                  </p>
+                  <h6>${car.price}<\h6>
+
+        </div>
+      </div>
+    </div>
+  </div>
+    `
+
+}
+function clearSearch(){
+    carDetail.innerHTML ="";
+    carDetail.style.display = "none";
+    main.style.display = "flex";
+    renderAllCars()
+}
